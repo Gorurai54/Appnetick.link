35e61505c410c1df7f6467dd6bd27e548fbcabba
Gorurai54 <goru78.nex@gmail.com>
2026-05-11 12:31:06 +0530

    Update verify-otp.js

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
       typeof req.body === "string"
         ? JSON.parse(req.body)
         : req.body || {};
+    console.log("RAW BODY:", req.body);
 
     let { email, otp } = body;
 
