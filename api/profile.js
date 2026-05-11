5c7dee8579037e4c5c84b6a92164354f5ad33b04
Gorurai54 <goru78.nex@gmail.com>
2026-05-11 07:43:05 +0530

    Update Open Graph description for user profile

--- api/profile.js
+++ api/profile.js
@@ -31,12 +31,11 @@ export default async function handler(req, res) {
 
         <link rel="icon" href="/20260314_091747.png">
 
-<meta property="og:image" content="https://appnetick-link.vercel.app/20260314_091747.png">
 <meta property="og:site_name" content="Appnetick">
 
         <!-- OG TAGS -->
         <meta property="og:title" content="${name}">
-        <meta property="og:description" content="${email}">
+        <meta property="og:description" content="${username} is on Appnetick • Find them in Appnetick App">
         <meta property="og:image" content="${avatar}">
         <meta property="og:type" content="profile">
 
