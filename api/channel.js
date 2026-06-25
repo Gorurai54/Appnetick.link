c92b6d7a62d99103e80618350da5319945aad420
Gorurai54 <goru78.nex@gmail.com>
2026-06-25 17:54:58 +0530

    Update channel.js

--- api/channel.js
+++ api/channel.js
@@ -27,15 +27,23 @@ export default async function handler(req, res) {
 
 <meta property="og:site_name" content="Appnetick">
 
+<meta property="og:site_name" content="Appnetick">
+
 <meta property="og:title" content="${name}">
 <meta property="og:description" content="${description}">
 <meta property="og:image" content="${logo}">
+<meta property="og:url" content="https://appnetick-link.vercel.app/user/channel/invite?uid=${req.query.uid}&cid=${cid}">
 <meta property="og:type" content="website">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="${name}">
+<meta name="twitter:description" content="${description}">
+<meta name="twitter:image" content="${logo}">
 
 <title>${name}</title>
 
 <meta http-equiv="refresh"
-content="0;url=/user/channel?uid=${req.query.uid}&cid=${cid}">
+content="0;url=/user/channel.html?uid=${req.query.uid}&cid=${cid}">
 
 </head>
 <body>
